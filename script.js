30528eca92fe7fd72cef7a6fc6c8af9d814863bb
Luk30lende <olendeluke2k@gmail.com>
2023-07-14 16:15:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const api = {
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
-const searchbox = document.querySelector('.search-box');
-searchbox.addEventListener('keypress', setQuery);
+const searchBox = document.querySelector('.search-box');
+searchBox.addEventListener('keypress', setQuery);
 
 function setQuery(evt) {
     if(evt.keyCode == 13) {
-        getResults(searchbox.value);
+        getResults(searchBox.value);
         // console.log(searchbox.value);
     }
 }
